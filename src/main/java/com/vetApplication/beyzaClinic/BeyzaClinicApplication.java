f238365797f3a447635968d306f21b0c85d02baf
Fiziyanali <beyzagumus64@gmail.com>
2021-12-29 17:26:20 +0300

    ..

--- src/main/java/com/vetApplication/beyzaClinic/BeyzaClinicApplication.java
+++ src/main/java/com/vetApplication/beyzaClinic/BeyzaClinicApplication.java
@@ -1,14 +1,8 @@
 package com.vetApplication.beyzaClinic;
 
-import com.vetApplication.beyzaClinic.model.entity.UserEntity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
 @SpringBootApplication
 public class BeyzaClinicApplication {
 
@@ -16,8 +10,4 @@ public class BeyzaClinicApplication {
 		SpringApplication.run(BeyzaClinicApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	public String index(UserEntity userEntity) {
-		return "login";
-	}
 }
