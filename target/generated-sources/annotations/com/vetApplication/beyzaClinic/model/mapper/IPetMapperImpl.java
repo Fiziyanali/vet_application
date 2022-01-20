b498f79ec0ddafe5df03a5e5313af1d2103058b0
Fiziyanali <beyzagumus64@gmail.com>
2022-01-20 10:33:47 +0300

    h2 db conf

--- target/generated-sources/annotations/com/vetApplication/beyzaClinic/model/mapper/IPetMapperImpl.java
+++ target/generated-sources/annotations/com/vetApplication/beyzaClinic/model/mapper/IPetMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-12-29T16:33:26+0300",
+    date = "2022-01-20T10:16:42+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.10 (JetBrains s.r.o.)"
 )
 @Component
